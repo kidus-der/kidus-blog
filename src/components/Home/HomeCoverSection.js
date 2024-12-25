30bff61564421966e52db95cafe44459eecc49e8
kidusdereje41@gmail.com <kidusdereje41@gmail.com>
2024-12-25 13:02:56 -0700

    feat: fixed error with no rendering of tag in cover blog post. Tag element created in src/components/Elements directory for modularity

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -2,6 +2,7 @@ import { sortBlogs } from "@/src/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Tag from "../Elements/Tag";
 
 const HomeCoverSection = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
@@ -12,8 +13,9 @@ const HomeCoverSection = ({ blogs }) => {
   return (
     <div className="w-full inline-block">
       <article className=" flex flex-col items-start justify-end mx-10 relative h-[85vh]">
-        <div className="absolute top-0 left-0 bottom-0 right-0 h-full
-        bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0" 
+        <div
+          className="absolute top-0 left-0 bottom-0 right-0 h-full
+        bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0"
         />
         <Image
           src={blog.image.filePath.replace("../public", "")}
@@ -25,15 +27,9 @@ const HomeCoverSection = ({ blogs }) => {
         />
 
         <div className="w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light">
-          <Link href={'/categories/${blog.tags[0]}'}>
-            {blog.tags[0]}
-          </Link>
-          <h1>
-            {blog.title}
-          </h1>
-          <p>
-            {blog.description}
-          </p>
+          <Tag link={"/categories/${blog.tags[0]}"} name={blog.tags[0]} />
+          <h1>{blog.title}</h1>
+          <p>{blog.description}</p>
         </div>
       </article>
     </div>
