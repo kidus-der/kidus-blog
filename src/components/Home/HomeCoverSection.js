1de5b2e0b4242a09ce58ca11ca6918ec86bd7d27
kidusdereje41@gmail.com <kidusdereje41@gmail.com>
2024-12-25 06:45:39 -0700

    feat: started to implement the cover post on the home page of the blog. currently the image of the blog has an overlay over it where the title of that blog will be

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -1,25 +1,34 @@
-import { sortBlogs } from '@/src/utils'
-import Image from 'next/image';
-import React from 'react'
+import { sortBlogs } from "@/src/utils";
+import Image from "next/image";
+import React from "react";
 
-const HomeCoverSection = ({blogs}) => {
+const HomeCoverSection = ({ blogs }) => {
+  const sortedBlogs = sortBlogs(blogs);
 
-    const sortedBlogs = sortBlogs(blogs);
-
-    // get the latest blog
-    const blog = sortedBlogs[0];
+  // get the latest blog
+  const blog = sortedBlogs[0];
 
   return (
-    <article className=' w-full flex flex-col items-start justify-end mx-10 relative h-[85vh]'>
-        <Image src={blog.image.filePath.replace("../public", "")}
-        placeholder='blur'
-        blurDataURL={blog.image.blurhashDataUrl}
-        alt={blog.title}
-        fill
-        className='w-full h-full object-center object-cover rounded-3xl'
+    <div className="w-full inline-block">
+      <article className=" flex flex-col items-start justify-end mx-10 relative h-[85vh]">
+        <div className="absolute top-0 left-0 bottom-0 right-0 h-full
+        bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0" 
         />
-    </article>
-  )
-}
+        <Image
+          src={blog.image.filePath.replace("../public", "")}
+          placeholder="blur"
+          blurDataURL={blog.image.blurhashDataUrl}
+          alt={blog.title}
+          fill
+          className="w-full h-full object-center object-cover rounded-3xl -z-10"
+        />
 
-export default HomeCoverSection
+        <div className="">
+
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default HomeCoverSection;
